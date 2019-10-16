e3fb48481b20ab55312c93331cd8d6e1787385e8
ali Rashad almanea <37377385+Alialmanea@users.noreply.github.com>
2019-10-16 13:53:36 -0700

    Update Move object in c language using Arrow keys.c

--- Move object in c language using Arrow keys.c
+++ Move object in c language using Arrow keys.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include<conio.h>
-# include<windows.h>
+#include<windows.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
@@ -11,44 +11,44 @@ int gotoxy(int x,int y)
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),coord);
 }
 
+int gotoxy(int x,int y);
 
-int main() {
+int main(){
 	
-	int x,y,i,j;
-	printf("Enter the x :");
-	scanf("%d", &x);
-	printf("Enter the :");
-	scanf("%d", &y);
+	int a,b,c,i;
+	char obj;
 	
+	a=5;
+	b=6;
 	
-	system("cls");
+  printf("Enter the object : ");
+  scanf("%c",&obj);
 	
-	
-	for(i=1;i<x;i++){//x
-		 gotoxy(i,y-1);
-		 printf(".");
-		for(j=x;j<y;j++){//y
-		    gotoxy(x-1,j);
-			printf(".");
-			
-		}	
-		 
-	}
-	
-	
+    system("cls");
+    gotoxy(a,b);
+    printf("%c",obj);
+
+while(1)
+{
+	if(kbhit())
+	{
+	c=getch();
+		if(c==77)
+		a++;
 		
-	for(i=1;i<x;i++){//x
-		 gotoxy(x,i);
-		 printf(".");
-		for(j=1;j<y;j++){//y
-		    gotoxy(j,y);
-			printf(".");
+		if(c==75)
+		a--;
+		
+		if(c==72)
+		b--;
 			
-		}	
-		 
-	}
-	
+		if(c==80)
+		b++;
+		
+     gotoxy(a,b);
+     printf("%c",obj);
 
-	
+	 }	
+}	
    getch();
 }
