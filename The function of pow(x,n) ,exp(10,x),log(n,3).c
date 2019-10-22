d0fa9015fad2192faa7ede6fcb6b5d53ab35171c
ali Rashad almanea <37377385+Alialmanea@users.noreply.github.com>
2019-10-22 12:36:28 -0700

    Update The function of pow(x,n) ,exp(10,x),log(n,3).c

--- The function of pow(x,n) ,exp(10,x),log(n,3).c
+++ The function of pow(x,n) ,exp(10,x),log(n,3).c
@@ -16,10 +16,43 @@ int i;
 
 
 
-float logn(float n,int r)
+/*float logn(float n,int r)
 {
 	return(n >r - 1 )?1+logn(n/r,r):0;
+}*/
+
+
+float mylog(float y)
+{
+ float x = (y - 1)/(y + 1);
+ double sum = x, term = x;
+ /* used for power */
+ double j = 3;
+ 
+ /* sums terms up to x^1200/1200 */
+ while(j < 1200)
+ {
+  j += 2;
+  
+  /* starts at term^3 */
+  term = (term * x * x);
+  
+  /* calcs term + term^3 to start */
+  sum = sum + (term / j);
+ }
+ 
+ /* finishes calc sum by multiplying by 2 */
+ sum = sum * 2;
+ 
+ /* if y is <= 0.0 return 0 */
+ if(y <= 0.0)
+ {
+  sum = 0;
+ }
+ 
+ return sum;
 }
+
 
 
 /* calculates power using previous function */
@@ -28,7 +61,7 @@ float mypow(float x, float p)
 {
  float result, k, answer;
     
- k = logn(x,3);
+ k = mylog(x,3);
  result = p * k;
  answer = exponential(10,result);
     
