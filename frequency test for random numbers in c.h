d9b6928b38fc0a5329626ba27782f882b9fe70f0
ali Rashad almanea <37377385+Alialmanea@users.noreply.github.com>
2019-11-06 16:42:12 +0300

    Add files via upload

---
+++ frequency test for random numbers in c.h
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int ebob(int a,int b)
+{
+	int mixnum,minnum;
+	if(a<b)
+	{
+		mixnum=a;
+		minnum=b;
+	}
+	else
+	{
+		mixnum=b;
+		minnum=a;
+	}
+	int bolen=minnum;
+	while(a%bolen!=0||b%bolen!=0)
+	{
+		bolen--;
+	}
+	return bolen;
+}
+
+int main(int argc, char *argv[]) {
+
+	int rd=ebob(75,60);
+	printf("%d",rd);
+
+
+	return 0;
+}
